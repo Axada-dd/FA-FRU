a328fbbc35218d778cafc7847803ee5c2cdf901b
denglang <99187696@qq.com>
2025-04-16 04:02:08 +0800

    优化逻辑，增加远程控制逻辑

--- FA-FRU/P1/P1-四连抓人记录.cs
+++ FA-FRU/P1/P1-四连抓人记录.cs
@@ -2,6 +2,7 @@ using AEAssist.CombatRoutine.Module.Target;
 using AEAssist.CombatRoutine.Trigger;
 using AEAssist.CombatRoutine.Trigger.Node;
 using AEAssist.Helper;
+using DDDacr.工具;
 
 namespace FA_FRU.P1;
 
@@ -13,16 +14,27 @@ public class P1_四连抓人记录 : ITriggerScript
         if (statusCondParams.StatusId != 1051) return false;
         var 点名目标 = TargetMgr.Instance.Units.Values
             .Where(e =>
-            {
-                foreach (var status in e.StatusList.ToList())
-                {
-                    if (status.StatusId == 1051) return true;
-                }
+                e.StatusList.ToList().Select(e => e.StatusId).ToList().Contains(1051)).ToList();
+        if (点名目标.Count == 1)
+        {
+            LogHelper.Print($"1号点名目标：{点名目标.Last().Name}，职能：{点名目标.Last().GetRoleByPlayerObjct()}");
+        }
 
-                return false;
-            }).ToList();
-        if(点名目标.Count != 4) return false;
+        if (点名目标.Count == 2)
+        {
+            LogHelper.Print($"2号点名目标：{点名目标.Last().Name}，职能：{点名目标.Last().GetRoleByPlayerObjct()}");    
+        }
+
+        if (点名目标.Count == 3)
+        {
+           LogHelper.Print($"3号点名目标：{点名目标.Last().Name}，职能：{点名目标.Last().GetRoleByPlayerObjct()}"); 
+        }
+
+        if (点名目标.Count == 4)
+        {
+           LogHelper.Print($"4号点名目标：{点名目标.Last().Name}，职能：{点名目标.Last().GetRoleByPlayerObjct()}");     
+        }
         LogHelper.Print("四连抓人记录: " + string.Join(",", 点名目标.Select(e => e.Name.ToString())));
-        return false;
+        return true;
     }
 }
