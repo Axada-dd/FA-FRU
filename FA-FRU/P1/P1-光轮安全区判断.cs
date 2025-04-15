a6ee1e64ca251a29a90979d6a20fc72765e9d991
denglang <99187696@qq.com>
2025-04-15 15:10:12 +0800

    更新正确逻辑

--- FA-FRU/P1/P1-光轮安全区判断.cs
+++ FA-FRU/P1/P1-光轮安全区判断.cs
@@ -15,9 +15,10 @@ public class P1_光轮安全区判断 : ITriggerScript
         var atEast = TargetMgr.Instance.Units.Values
             .Where(u => u.IsCasting && u.CastActionId == 40152 && (MathF.Abs(u.Position.Z - 100) < 1)).ToList()[0]
             .Position.X-100>1;
-        if (!scriptEnv.KV.ContainsKey("P1光轮左安全"))
+        if (!scriptEnv.KV.ContainsKey("P1光轮右安全"))
         {
-            scriptEnv.KV.Add("P1光轮左安全", atEast);
+            scriptEnv.KV.Add("P1光轮右安全", atEast);
+            LogHelper.Print($"P1光轮右安全区判断: {atEast}");
         }
         return true;
     }
--- FA-FRU/P1/P1-光轮换位.cs
+++ FA-FRU/P1/P1-光轮换位.cs
@@ -9,17 +9,22 @@ public class P1_光轮换位 : ITriggerScript
 {
     public bool Check(ScriptEnv scriptEnv, ITriggerCondParams condParams)
     {
-        if (!scriptEnv.KV.ContainsKey("P1光轮左安全")) return false;
         if(condParams is not AddStatusCondParams statusCondParams) return false;
         if (statusCondParams.StatusId != 1051) return false;
         var 点名目标 = TargetMgr.Instance.Units.Values
             .Where(e =>
             {
-                using var enumerator = e.StatusList.GetEnumerator();
-                return enumerator.Current.StatusId == 1051;
+                foreach (var status in e.StatusList.ToList())
+                {
+                    if (status.StatusId == 1051) return true;
+                }
+
+                return false;
             })
-            .Select(e => e.Name.ToString());
+            .Select(e => e.Name.ToString()).ToList();
         LogHelper.Print("点名目标: " + string.Join(",", 点名目标));
-        return false;
+        if(点名目标.Count != 2) return false;
+        if (!scriptEnv.KV.ContainsKey("P1光轮左安全")) return false;
+        return true;
     }
 }
