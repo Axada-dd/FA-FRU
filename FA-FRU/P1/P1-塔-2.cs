ec01d5d5823b820075e449f4a9674a77aeaee0e0
denglang <99187696@qq.com>
2025-04-19 00:57:04 +0800

    P1踩塔逻辑完成

---
+++ FA-FRU/P1/P1-塔-2.cs
@@ -0,0 +1,22 @@
+using System.Numerics;
+using AEAssist.CombatRoutine.Trigger;
+using AEAssist.CombatRoutine.Trigger.Node;
+using AEAssist.Helper;
+
+namespace FA_FRU.P1;
+
+public class P1_塔_2 : ITriggerScript
+{
+    public bool Check(ScriptEnv scriptEnv, ITriggerCondParams condParams)
+    {
+        if(condParams is not ReceviceNoTargetAbilityEffectCondParams noTargetAbilityEffectCondParams) return false;
+        if (noTargetAbilityEffectCondParams.ActionId is not (40129 or 40134)) return false;
+        if (!scriptEnv.KV.ContainsKey("P1踩塔")) return false;
+        var 踩塔 = (List<KeyValuePair<string, Vector3>>) scriptEnv.KV["P1踩塔"];
+        foreach (var action in 踩塔)
+        {
+            RemoteControlHelper.SetPos(action.Key,action.Value);
+        }
+        return true;
+    }
+}
--- FA-FRU/P1/P1-塔记录.cs
+++ FA-FRU/P1/P1-塔记录.cs
@@ -9,11 +9,13 @@ namespace FA_FRU.P1;
 public class P1_塔记录 : ITriggerScript
 {
     List<int> P1塔 = [0, 0, 0, 0];
+    private int _times = 0;
     public bool Check(ScriptEnv scriptEnv, ITriggerCondParams condParams)
     {
         if(condParams is not EnemyCastSpellCondParams spellCondParams) return false;
         Regex myRegex = new Regex(@"(4012[234567]|4013[15])");
         if (!myRegex.IsMatch(spellCondParams.SpellId.ToString())) return false;
+        _times++;
         var pos = spellCondParams.CastPos;
         var count = spellCondParams.SpellId switch
         {
@@ -50,6 +52,8 @@ public class P1_塔记录 : ITriggerScript
             LogHelper.Print($"右塔");
         }
         LogHelper.Print(P1塔.Print());
-        return false;
+        if (_times != 3) return false;
+        if(!scriptEnv.KV.ContainsKey("P1塔")) scriptEnv.KV.Add("P1塔", P1塔);
+        return true;
     }
 }
